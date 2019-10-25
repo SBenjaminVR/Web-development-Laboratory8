adab7746ba2858050848fdc9f55cf91939986e22
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2019-10-24 20:19:02 -0500

    Added get requests: one to get all posts and another one to getall the posts of an author

--- server.js
+++ server.js
@@ -29,11 +29,48 @@ let blogPosts = [
     },
     {
         id: uuid.v4(),
+        title: "Tusk ACT 3",
+        content: "By shooting himself with a Spin-imbued fingernail with aureal rotation, Johnny is able to suck his body into the hole it creates.",
+        author: "Johnny Joestar",
+        publishDate: new Date("January 19, 1891, 9:22:12")
+    },
+    {
+        id: uuid.v4(),
         title: "Tusk ACT 4",
         content: "Tusk ACT4 is the highest evolution of Tusk, accessed only when Johnny uses the Golden Spin.",
         author: "Johnny Joestar",
         publishDate: new Date("January 19, 1891, 10:25:19")
+    }
+];
 
-    },
-    
-]
+app.get('/blog-posts', (req, res, next) => {
+    return res.status(200).json(blogPosts);
+})
+
+app.get('/blog-post', (req, res, next) => {
+    if (req.query.author == undefined) {
+        res.statusMessage = "Author param is missing"
+        return res.status(406).json({
+            code: 406,
+            message: "Author param is missing"
+        })
+    }
+    let authorPosts = []
+    for (let i = 0; i < blogPosts.length; i++) {
+        if (req.query.author ==  blogPosts[i].author) {
+            authorPosts.push(blogPosts[i]);
+        }
+    }
+    if (authorPosts.length < 1) {
+        res.statusMessage = "Author doesn't exist";
+        return res.status(404).json({
+            code: 404,
+            message: "Author doesn't exist"
+        });
+    }
+    return res.status(200).json(authorPosts);
+});
+
+app.listen('8080', () => {
+    console.log("App running on localhost:8080");
+});
